7718f3a76bb4fb66e3c605b5193d23a59ada2eb6
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2024-03-18 19:54:54 +0400

    Add project files.

---
+++ ClientSide/MainWindow.xaml.cs
@@ -0,0 +1,210 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace ClientSide;
+
+
+public partial class MainWindow : Window
+{
+    public UdpClient client;
+    public IPAddress remoteIP;
+    public int remotePort;
+    public IPEndPoint remoteEP;
+
+    public bool isCheck = false;
+    public bool isFirst = true;
+
+    bool isBreak = false;
+
+    bool isRecord = false;
+    public static BigInteger counter = 0;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        remoteIP = IPAddress.Parse("127.0.0.1");
+        remotePort = 27001;
+        remoteEP = new IPEndPoint(remoteIP, remotePort);
+
+        client = new UdpClient();
+    }
+
+    //private async void Button_Click(object sender, RoutedEventArgs e)
+    //{
+    //    if (!isCheck)
+    //    {
+    //        isCheck = true;
+    //        var buffer = new byte[ushort.MaxValue - 29];
+
+
+    //        if (isFirst)
+    //        {
+    //            await client.SendAsync(buffer, buffer.Length, remoteEP);
+    //            isFirst = false;
+    //        }
+
+    //        var list = new List<byte>();
+    //        var maxLen = buffer.Length;
+    //        var len = 0;
+
+    //        while (true)
+    //        {
+    //            if (isBreak)
+    //                break;
+
+
+    //            do
+    //            {
+    //                try
+    //                {
+    //                    var result = await client.ReceiveAsync();
+
+    //                    buffer = result.Buffer;
+    //                    len = buffer.Length;
+    //                    list.AddRange(buffer);
+    //                }
+    //                catch (Exception ex)
+    //                {
+    //                    System.Windows.MessageBox.Show(ex.Message);
+    //                }
+
+    //            } while (len == maxLen);
+
+    //            var image = await ByteToImageAsync(list.ToArray());
+    //            if (image is not null)
+    //                ScreenImage.Source = image;
+
+    //            list.Clear();
+    //        }
+    //        isBreak = false;
+    //    }
+    //}
+
+
+    public async Task<BitmapImage> ByteToImageAsync(byte[] bytes)
+    {
+        var image = new BitmapImage();
+        image.BeginInit();
+
+
+        //if (isRecord)
+        //{
+        //    var ms = new MemoryStream(bytes);
+        //    var imagePath = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\Step\\image (" + counter + ").jpeg";
+
+        //    FileStream file = new FileStream(imagePath, FileMode.Create, FileAccess.Write);
+        //    await ms.CopyToAsync(file);
+        //    counter++;
+        //}
+
+
+        image.StreamSource = new MemoryStream(bytes);
+        image.CacheOption = BitmapCacheOption.OnLoad;
+
+        image.EndInit();
+        return image;
+    }
+
+
+    private void ConnectToServer(object sender, RoutedEventArgs e)
+    {
+        var buffer = new byte[ushort.MaxValue - 29];
+        client.SendAsync(buffer, buffer.Length, remoteEP);
+
+
+    }
+
+    private async void SendToScreenshot(object sender, RoutedEventArgs e)
+    {
+
+        var screen = await TakeScreenShotAsync();
+        var imageBytes = await ImageToByteAsync(screen);
+
+        var chunks = imageBytes.Chunk(ushort.MaxValue - 29);
+
+        foreach (var chunk in chunks)
+            await client.SendAsync(chunk, chunk.Length, remoteEP);
+
+
+    }
+
+    private async void ShowScreen(object sender, RoutedEventArgs e)
+    {
+        if (!isCheck)
+        {
+            isCheck = true;
+            var buffer = new byte[ushort.MaxValue - 29];
+
+
+            //if (isFirst)
+            //{
+            //    await client.SendAsync(buffer, buffer.Length, remoteEP);
+            //    isFirst = false;
+            //}
+
+            var list = new List<byte>();
+            var maxLen = buffer.Length;
+            var len = 0;
+
+            while (true)
+            {
+                //if (isBreak)
+                //    break;
+
+
+                do
+                {
+                    try
+                    {
+                        var result = await client.ReceiveAsync();
+
+                        buffer = result.Buffer;
+                        len = buffer.Length;
+                        list.AddRange(buffer);
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Windows.MessageBox.Show(ex.Message);
+                    }
+
+                } while (len == maxLen);
+
+                var image = await ByteToImageAsync(list.ToArray());
+                if (image is not null)
+                    ScreenImage.Source = image;
+
+                list.Clear();
+            }
+            // isBreak = false;
+        }
+    }
+
+    async Task<Image> TakeScreenShotAsync()
+    {
+        var width = Screen.PrimaryScreen.Bounds.Width;
+        var height = Screen.PrimaryScreen.Bounds.Height;
+        Bitmap bitmap = new Bitmap(width, height);
+
+        using Graphics? g = Graphics.FromImage(bitmap);
+        g?.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+        return bitmap;
+    }
+
+
+    async Task<byte[]> ImageToByteAsync(Image image)
+    {
+        using MemoryStream ms = new MemoryStream();
+        image.Save(ms, ImageFormat.Jpeg);
+
+        return ms.ToArray();
+    }
+}
+
---
+++ ServerSide/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Text;
+
+
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+
+var listenerEP = new IPEndPoint(ip, port);
+//var remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+var listener = new UdpClient(listenerEP);
+// --------------------------------------------------------------------------------------
+
+List<UdpClient> Clients = new List<UdpClient>();
+
+while (true)
+{
+
+    var resultt = await listener.ReceiveAsync();
+    Console.WriteLine($"{resultt.RemoteEndPoint} Connected...");
+
+    if (Database.RemoteEPS.Contains(resultt.RemoteEndPoint) == false)
+    {
+
+        Database.RemoteEPS.Add(resultt.RemoteEndPoint);
+    }
+
+    Console.WriteLine(Database.RemoteEPS.Count);
+
+
+    while (true)
+    {
+
+        var buffer = new byte[ushort.MaxValue - 29];
+        var list = new List<byte>();
+        var maxLen = buffer.Length;
+        var len = 0;
+
+        do
+        {
+            try
+            {
+                var result = await listener.ReceiveAsync();
+
+                buffer = result.Buffer;
+                len = buffer.Length;
+                list.AddRange(buffer);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        } while (len == maxLen);
+
+        if (Database.RemoteEPS.Count > 0)
+        {
+            foreach (var item in Database.RemoteEPS)
+            {
+                //Console.WriteLine(item.ToString());
+                var chunks = list.ToArray().Chunk(maxLen);
+                foreach (var chunk in chunks)
+                {
+                    await listener.SendAsync(chunk, chunk.Length, item);
+                }
+            }
+            list.Clear();
+
+        }
+
+    }
+
+
+}
+
+public static class Database
+{
+    public static List<IPEndPoint> RemoteEPS = new List<IPEndPoint>();
+}
+
+
+
+//var result = await listener.ReceiveAsync();
+//Console.WriteLine($"{result.RemoteEndPoint} .. Connected ...");
+//Clients.Add(listener);
+
+//_ = Task.Run(async () =>
+//{
+//    foreach (var item in Clients)
+//    {
+//        var remoteEP = result.RemoteEndPoint;
+
+//        while (true)
+//        {
+//            var screen = await TakeScreenShotAsync();
+//            var imageBytes = await ImageToByteAsync(screen);
+
+//            var chunks = imageBytes.Chunk(ushort.MaxValue - 29);
+
+//            foreach (var chunk in chunks)
+//                await listener.SendAsync(chunk, chunk.Length, remoteEP);
+//        }
+
+//    }
+//});
+//// --------------------------------------------------------------------------------------
+//// Ekranin Screen edilme hissesi.
+
+//async Task<Image> TakeScreenShotAsync()
+//{
+//    var width = Screen.PrimaryScreen.Bounds.Width;
+//    var height = Screen.PrimaryScreen.Bounds.Height;
+//    Bitmap bitmap = new Bitmap(width, height);
+
+//    using Graphics? g = Graphics.FromImage(bitmap);
+//    g?.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+//    return bitmap;
+//}
+
+
+//async Task<byte[]> ImageToByteAsync(Image image)
+//{
+//    using MemoryStream ms = new MemoryStream();
+//    image.Save(ms, ImageFormat.Jpeg);
+
+//    return ms.ToArray();
+//}
+
